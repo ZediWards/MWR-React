46e7a40a2547c3ef52817b10d249f43bf73575e9
zediwards <zediwards@protonmail.com>
2021-11-30 12:34:00 -0500

    starting components

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+const Footer = () => {
+  return (
+    <div>
+      <p>The footer</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+
+
+const Header = () => {
+  return (
+    <div>
+      <p>Your logo here</p>
+    </div>
+  )
+};
+
+export default Header;
