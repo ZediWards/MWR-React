74498ff513cb9581403dcbb1f397e83ea007bbd4
zediwards <zediwards@protonmail.com>
2022-04-23 01:00:54 -0400

    start of template for pdf generator

---
+++ src/components/pdfTemplate.js
@@ -0,0 +1,41 @@
+import * as React from "react"
+
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Image
+} from "@react-pdf/renderer"
+// import { PDFViewer } from "@react-pdf/renderer";
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "row",
+    backgroundColor: "#E4E4E4"
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+})
+
+// Create Document Component
+const PdfDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        {/* <Text>{props.data.status}</Text> */}
+        <Text>hello</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+)
+
+export default PdfDocument
