d54200250a0806394806a3f4d7d81b3641051528
zediwards <zediwards@protonmail.com>
2022-06-20 02:54:24 -0400

    notes in a way to push to github

---
+++ src/components/notes.js
@@ -0,0 +1,49 @@
+// <!-- ********************* Do Now ******************** -->
+// [x] pass the updatePrimaryFilter function down from index to Primary filter btns
+
+// [x] change new mwr for to default to lowerCase undefined
+// [x] apply css classNames to updatePrimaryFilter funtion
+
+// [x] make request # on details modal = mwr id
+// [x] change details status to lowerCase and 
+// [x] what you want primary filters to be.
+// [X] details screen undefiened needs to be -
+//         - problem type, assigned to, maint. team member, assistant
+  
+// [] why is prob/col in details a lighter text than other disabled fields?
+
+// [] mwr types in details dropdown not lowercase. is it updating as lowercase?
+
+// [] organize details modal
+//         [] get rid of project #?
+//         [] get rid of problem type? ideas: electrical, plumbing,...?
+// [] job status on own row at top of mantenence section
+// [] due date setion move below assigned section
+// [] brief/work discriptions move under department / site section
+
+// [] take awway white text color on mwr for close button. Just pointer on hover is needed
+
+// [] get rid of input box border/shadows
+
+// [] make dropdowns box the same height as input boxes
+
+// [] focus color on input boxes
+
+// [] change table hover color to part of color scheme
+//         [] make all colors hue be linked to main settings mwr colors. So they will match if colors changed
+
+// [] clean out mwrForm fields and db fields to match and use just what is needed
+// <!-- ***********************Do later******************** -->
+
+// [] close all modals by clicking outside of modal
+
+// <!-- ***********************Do sometime******************* -->
+
+// [] settings page
+//         [] gear icon somewhere to open settings
+
+// [] seperate and import states into idex from their own file
+
+// [] does my use case for useEffect neeed cleanup?
+
+// [] need auth to edit details modal
