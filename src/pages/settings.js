b251f3970b7fed415b3e30019ebe4807af7e736c
zediwards <zediwards@protonmail.com>
2022-10-05 02:55:57 -0400

    control btn visability working

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -67,6 +67,21 @@ const SettingsWrapperStyled = styled.div`
     background-color: hsl(var(--safety-mwr-hue), 50%, 90%);
   }
 
+  .edit-btn {
+  }
+
+  .add-new-btn {
+    /* display: none; */
+  }
+
+  .cancel-btn {
+    /* display: none; */
+  }
+
+  .save-btn {
+    /* display: none; */
+  }
+
   /* --gray-light from global css need a primary-hue declared to work */
   .delete-btn {
     /* align-self: flex-start; */
@@ -95,6 +110,10 @@ const SettingsWrapperStyled = styled.div`
     /* background-color: blue; */
     display: block;
   }
+
+  .display-none {
+    display: none;
+  }
 `;
 
 const SettingsPage = () => {
@@ -113,31 +132,115 @@ const SettingsPage = () => {
 
   //*************? Edit functionality **************/
 
-  // enabling edit function
-  const enableEdit = (e) => {
+  // TODO:
+  // [x] edit: hide when clicked, show other btns
+  // [x] cancel, save: hide all btns and show edit
+  // [x] make delete btn display: none as well && inputs enabled at the right time
+  // [ ] addNew: enable input for editing and delete btn visiable
+
+  // Show Hide Function
+  const showHideBtns = (e, btnType) => {
+    // DOM variables
+    const parentDiv = e.target.closest("div");
+    const editBtn = parentDiv.querySelector(".edit-btn");
+    const addNewBtn = parentDiv.querySelector(".add-new-btn");
+    const cancelBtn = parentDiv.querySelector(".cancel-btn");
+    const saveBtn = parentDiv.querySelector(".save-btn");
+
     const category = e.target.closest("li");
-
     const categoryList = category.querySelector("ul");
-
     const listItems = categoryList.children;
 
-    // display block on delete btn **all but mwr type**
-    for (let listItem of listItems) {
-      // console.log(listItem.children[0].children[0].disabled);
+    switch (btnType) {
+      // update settings section
+      case "edit": {
+        editBtn.className = "control-btns edit-btn display-none";
+        addNewBtn.className = "control-btns add-new-btn";
+        cancelBtn.className = "control-btns cancel-btn";
+        saveBtn.className = "control-btns save-btn";
 
-      let btnTarget = listItem.children[0].children[1].children[0];
-      btnTarget.className !== "delete-btn btn-visibile"
-        ? (btnTarget.className = "delete-btn btn-visibile")
-        : (btnTarget.className = "delete-btn");
+        // inputs and delete btns
+        // display block on delete btn **all but mwr type**
+        for (let listItem of listItems) {
+          // console.log(listItem.children[0].children[0].disabled);
 
-      // enable inputs for editing
-      let inputTarget = listItem.children[0].children[0];
-      inputTarget.disabled === true
-        ? (inputTarget.disabled = false)
-        : (inputTarget.disabled = true);
+          let btnTarget = listItem.children[0].children[1].children[0];
+          btnTarget.className !== "delete-btn btn-visibile"
+            ? (btnTarget.className = "delete-btn btn-visibile")
+            : (btnTarget.className = "delete-btn");
 
-      // *** next step is to allow onChange event to change the settings context
+          // enable inputs for editing
+          let inputTarget = listItem.children[0].children[0];
+          inputTarget.disabled === true
+            ? (inputTarget.disabled = false)
+            : (inputTarget.disabled = true);
+
+          // *** next step is to allow onChange event to change the settings context
+        }
+        break;
+      }
+      case "cancel":
+      case "save":
+        editBtn.className = "control-btns edit-btn";
+        addNewBtn.className = "control-btns add-new-btn display-none";
+        cancelBtn.className = "control-btns cancel-btn display-none";
+        saveBtn.className = "control-btns save-btn display-none";
+
+        // inputs and delete btns
+        // display block on delete btn **all but mwr type**
+        for (let listItem of listItems) {
+          // console.log(listItem.children[0].children[0].disabled);
+          // Don't really need terniary but am keeping for a reference
+          let btnTarget = listItem.children[0].children[1].children[0];
+          btnTarget.className !== "delete-btn"
+            ? (btnTarget.className = "delete-btn")
+            : (btnTarget.className = "delete-btn btn-visibile");
+
+          // enable inputs for editing
+          let inputTarget = listItem.children[0].children[0];
+          inputTarget.disabled === false
+            ? (inputTarget.disabled = true)
+            : (inputTarget.disabled = false);
+
+          // *** next step is to allow onChange event to change the settings context
+        }
+        break;
+
+      // case ACTIONS.DELETE_MWR: {
+      //   return state.filter((mwr) => mwr.id !== action.payload.id);
+      // }
+      default:
+        throw new Error("Bad Action Type");
+      // or return original state
+      // default:
+      // return state
     }
+  };
+
+  // enabling edit function
+  const enableEdit = (e) => {
+    // const category = e.target.closest("li");
+    // const categoryList = category.querySelector("ul");
+    // const listItems = categoryList.children;
+    showHideBtns(e, "edit");
+
+    // // display block on delete btn **all but mwr type**
+    // for (let listItem of listItems) {
+    //   // console.log(listItem.children[0].children[0].disabled);
+
+    //   let btnTarget = listItem.children[0].children[1].children[0];
+    //   btnTarget.className !== "delete-btn btn-visibile"
+    //     ? (btnTarget.className = "delete-btn btn-visibile")
+    //     : (btnTarget.className = "delete-btn");
+
+    //   // enable inputs for editing
+    //   let inputTarget = listItem.children[0].children[0];
+    //   inputTarget.disabled === true
+    //     ? (inputTarget.disabled = false)
+    //     : (inputTarget.disabled = true);
+
+    //   // *** next step is to allow onChange event to change the settings context
+    // }
   };
 
   // condition ? value if true : value if false
@@ -250,6 +353,8 @@ const SettingsPage = () => {
   //! WORKS
   const cancelChanges = (e, item) => {
     e.stopPropagation();
+    showHideBtns(e, "cancel");
+
     const newArr = settings[item].slice();
     console.table(newArr);
     setUpdateSettings((current) => {
@@ -262,15 +367,20 @@ const SettingsPage = () => {
   //***************? Save to Context Local Storage Function **************************/
   const saveSection = (e, item) => {
     e.preventDefault();
+    showHideBtns(e, "save");
+
     const newArr = updateSettings[item].slice();
     const updatedSettings = {
       section: item,
       data: newArr
-    }
+    };
     console.log("overriding context local storage");
-    console.log(updatedSettings)  //payload
+    console.log(updatedSettings); //payload
     // settingsDispatch({ type: ACTIONS.UPDATE_SETTINGS, payload: updateSettings });
-    settingsDispatch({ type: ACTIONS.UPDATE_SETTINGS_SECTION, payload: updatedSettings });
+    settingsDispatch({
+      type: ACTIONS.UPDATE_SETTINGS_SECTION,
+      payload: updatedSettings
+    });
   };
 
   //? *********** Mapping JSX **********************
@@ -331,22 +441,27 @@ const SettingsPage = () => {
           <div className={"category-header-container"}>
             <h2 className={"category-header"}>mwr types</h2>
             <div className={"category-controls-container"}>
-              <span className={"control-btns"} onClick={enableEdit}>
+              <span className={"control-btns edit-btn"} onClick={enableEdit}>
                 edit
               </span>
               <span
-                className={"control-btns"}
+                className={"control-btns add-new-btn display-none"}
                 onClick={(e) => addItem(e, item)}
               >
                 add new
               </span>
               <span
-                className={"control-btns"}
+                className={"control-btns cancel-btn display-none"}
                 onClick={(e) => cancelChanges(e, item)}
               >
                 cancel
               </span>
-              <span className={"control-btns"} onClick={(e) => saveSection(e, item)}>save</span>
+              <span
+                className={"control-btns save-btn display-none"}
+                onClick={(e) => saveSection(e, item)}
+              >
+                save
+              </span>
             </div>
           </div>
           <ul>{mwrTypeSettingsMap}</ul>
@@ -358,22 +473,27 @@ const SettingsPage = () => {
           <div className={"category-header-container"}>
             <h2 className={"category-header"}>{item}</h2>
             <div className={"category-controls-container"}>
-              <span className={"control-btns"} onClick={enableEdit}>
+              <span className={"control-btns edit-btn"} onClick={enableEdit}>
                 edit
               </span>
               <span
-                className={"control-btns"}
+                className={"control-btns add-new-btn display-none"}
                 onClick={(e) => addItem(e, item)}
               >
                 add new
               </span>
               <span
-                className={"control-btns"}
+                className={"control-btns cancel-btn display-none"}
                 onClick={(e) => cancelChanges(e, item)}
               >
                 cancel
               </span>
-              <span className={"control-btns"} onClick={(e) => saveSection(e, item)}>save</span>
+              <span
+                className={"control-btns save-btn display-none"}
+                onClick={(e) => saveSection(e, item)}
+              >
+                save
+              </span>
             </div>
           </div>
           <ul>
