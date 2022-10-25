6479b8d9fba2431840484160e140726f2d65d218
zediwards <zediwards@protonmail.com>
2022-10-25 04:07:07 -0400

    starting color picker for settings

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import GlobalContextProvider from './src/context/GlobalContextProvider'
-// import GlobalStylesDiv from './src/global_styles/globalStyledComponents'
+import GlobalStylesDiv from './src/global_styles/globalStyledComponents'
 
 import "./src/global_styles/global.css"
 
 export const wrapRootElement = ({ element }) => {
   return <GlobalContextProvider>
-    {/* <GlobalStylesDiv> */}
-    {element}
-    {/* </GlobalStylesDiv> */}
+    <GlobalStylesDiv>
+      {element}
+    </GlobalStylesDiv>
   </GlobalContextProvider>
 }
 
--- src/global_styles/globalStyledComponents.js
+++ src/global_styles/globalStyledComponents.js
@@ -1,68 +1,50 @@
-// import * as React from "react"
-// import { useContext } from "react";
+import * as React from "react"
+import { useContext } from "react";
 
-// import styled from "styled-components";
+import styled from "styled-components";
 
-// import {
-//   GlobalSettingsContext,
-// } from "../context/GlobalContextProvider";
+import {
+  GlobalSettingsContext,
+} from "../context/GlobalContextProvider";
 
-// // Lesson: Wrapping gatsby with a styled component 
-// // **** Styled Components ****
-
-
+// Lesson: Wrapping gatsby with a styled component 
+// Lesson: had to put styled components inside component function to be able to access useContext
 
 
-// const GlobalStyleWrapper = styled.div`
-//   /* starting hues */
-//   --general-mwr-hue: 111;
-//   --safety-mwr-hue: 2;
-//   --urgent-mwr-hue: 52;
+const GlobalStylesDiv = ({ children }) => {
+  const settings = useContext(GlobalSettingsContext)
 
-// /* hex convert to hsl: https://stackoverflow.com/questions/46432335/hex-to-hsl-convert-javascript */
-//   /* colors based on hues */
-//   --background-general: hsl(var(--general-mwr-hue), 50%, 85%);
-//   --background-safety: hsl(var(--safety-mwr-hue), 50%, 85%);
-//   --background-urgent: hsl(var(--urgent-mwr-hue), 50%, 85%);
+  // Convert hex to hsl and grab the hue
+  // could use color picker to get hue then apply that to the background
+  // https://stackoverflow.com/questions/3732046/how-do-you-get-the-hue-of-a-xxxxxx-colour
+  const GlobalStyleWrapper = styled.div`
+  /* starting hues */
+  --general-mwr-hue: 111;
+  --safety-mwr-hue: 2;
+  --urgent-mwr-hue: 52;
 
-//   --background-general: ${mwrTypeColors};
+/* hex convert to hsl: https://stackoverflow.com/questions/46432335/hex-to-hsl-convert-javascript */
+  /* colors based on hues */
+  /* --background-general: hsl(var(--general-mwr-hue), 50%, 85%); */
+  --background-general: ${settings.mwrTypes[0].color};
+
+  /* --background-urgent: hsl(var(--urgent-mwr-hue), 50%, 85%); */
+  --background-urgent: ${settings.mwrTypes[1].color};
+
+  /* --background-safety: hsl(var(--safety-mwr-hue), 50%, 85%); */
+  --background-safety: ${settings.mwrTypes[2].color}
   
-//   `
-// const color1 = settings.mwrTypes[0].color
-// const mwrTypeColor1 = (color1) => {
-//   if (color1 === ) {
+  `
+  return (
+    <GlobalStyleWrapper>
+      {children}
+      {/* {mwrTypeColors()} */}
+    </GlobalStyleWrapper>
+  )
 
-//   }
-// }
-// // background-color: ${(prop) =>
-// //   prop.mwrType.type === settings.mwrTypes[0]
-// //     ? "var(--background-general)"
-// //     : prop.mwrType.type === "Safety"
-// //       ? "var(--background-safety)"
-// //       : "var(--background-urgent)"};
-// // `;
-
-// const GlobalStylesDiv = ({ children }) => {
-//   const settings = useContext(GlobalSettingsContext)
-//   const mwrTypeColors = (settings) => {
-//     switch ("dfa") {
-//       // update settings section
-//       case settings.mwrTypes[0].type: {
-//         if (settings.mwrTypes[0].color === "#00ff24")
-//           break;
-//       }
-//       // console.log(settings)
-//     }
-//     return (
-//       <GlobalStyleWrapper>
-//         {children}
-//         {mwrTypeColors()}
-//       </GlobalStyleWrapper>
-//     )
-
-//   }
-// }
+}
 
 
 
-// export default GlobalStylesDiv
+
+export default GlobalStylesDiv
