b10f210399e18a9dccd58134a2c17ea4ab262523
zediwards <zediwards@protonmail.com>
2022-11-15 01:46:59 -0500

    pisum placeholder images

--- src/components/LightGallery.js
+++ src/components/LightGallery.js
@@ -30,58 +30,63 @@ function Gallery() {
       elementClassNames="light-gallery-wrapper"
       // onInit={onInit}
       // onBeforeSlide={onBeforeSlide}
+      // speed="4000"
       plugins={[lgZoom]}
-      mode="lg-fade"
+      // mode="lg-fade"
+      mode="lg-slide-skew-cross"
     >
       {/* <a href="img/img1.jpg">
           <img alt="img1" src="img/thumb1.jpg" />
         </a> */}
-
       <a
-        data-lg-size="1406-1390" // needed for lgZoom plugin, uses default if not provided
+        data-lg-size="5000-3224" // needed for lgZoom plugin, uses default if not provided
         className="gallery-item"
-        data-src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80"
+        data-src="https://picsum.photos/id/297/5000/3224"
         data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@entrycube' >Diego Guzmán </a></h4> <p> Location - <a href='https://unsplash.com/s/photos/fushimi-inari-taisha-shrine-senbontorii%2C-68%E7%95%AA%E5%9C%B0-fukakusa-yabunouchicho%2C-fushimi-ward%2C-kyoto%2C-japan'>Fushimi Ward, Kyoto, Japan</a></p>"
       >
         <img
           // className="img-responsive"
-          src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
+          src="https://picsum.photos/id/297/5000/3224"
         />
       </a>
-
       <a
-        data-lg-size="1400-1400"
+        data-lg-size="1500-1000"
         data-pinterest-text="Shinimamiya, Osaka, Japan"
         data-tweet-text="Shinimamiya, Osaka, Japan"
         className="gallery-item"
-        data-src="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
+        data-src="https://picsum.photos/id/115/1500/1000.jpg"
         data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@asoshiation' >Shah </a></h4><p> Location - <a href='https://unsplash.com/s/photos/shinimamiya%2C-osaka%2C-japan'>Shinimamiya, Osaka, Japan</a></p>"
       >
         <img
           // className="img-responsive"
-          src="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
+          src="https://picsum.photos/id/115/1500/1000"
         />
       </a>
-
       <a
-        data-lg-size="1400-1400"
+        data-lg-size="5000-2917"
         className="gallery-item"
-        data-src="https://images.unsplash.com/photo-1584592740039-cddf0671f3d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
+        data-src="https://picsum.photos/id/364/5000/2917"
         data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
       >
         <img
           // className="img-responsive"
-          src="https://images.unsplash.com/photo-1584592740039-cddf0671f3d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
+          src="https://picsum.photos/id/364/5000/2917"
         />
       </a>
-
+      <a
+        data-lg-size="5000-3333"
+        className="gallery-item"
+        data-src="https://picsum.photos/id/1060/5000/3333.jpg"
+        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
+      >
+        <img
+          // className="img-responsive"
+          src="https://picsum.photos/id/1060/5000/3333.jpg"
+        />
+      </a>
+      {/* img styles: width: 100%; height: 100px; object-fit: cover;
+      object-position: bottom; cursor: pointer */}
       {/* ******************** */}
-      {/* <a href="img/img1.jpg">
-          <img alt="img1" src="img/thumb1.jpg" />
-        </a>
-        <a href="img/img1.jpg">
-          <img alt="img1" src="img/thumb1.jpg" />
-        </a> */}
     </LightGallery>
   );
 }
