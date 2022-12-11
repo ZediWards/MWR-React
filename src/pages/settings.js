ba3cf07a3f18f525e5af0b393b4329977e886ee0
zediwards <zediwards@protonmail.com>
2022-12-11 03:02:17 -0500

    nav styles now styled components

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -250,6 +250,44 @@ const SettingsWrapperStyled = styled.div`
   }
 `;
 
+const Navbar = {
+  Wrapper: styled.nav`
+    margin-top: -1.45rem;
+    margin-bottom: 0.5rem;
+    max-width: 963px;
+    
+    /* from article */
+    /* padding: 1rem 3rem; */
+    /* align-items: center; */
+  `,
+
+  Items: styled.ul`
+    display: flex;
+    gap: "2rem";
+    /* justify-content: end; */
+    /* center on mobile */
+    justify-content: center;
+    align-items: center;
+    list-style-type: none;
+    margin: auto;
+  `,
+
+  Item: styled.li`
+    padding: 0.25rem 0.5rem;
+    margin: 0;
+    cursor: pointer;
+    /* article */
+    /* padding: 0 1rem; */
+  `,
+  ItemIsActivePage: styled.li`
+    padding: 0.25rem 0.5rem;
+    margin: 0;
+    cursor: pointer;
+    text-decoration: underline var(--red-btn-color-darker) 2px;
+
+  `
+}
+
 const SettingsPage = () => {
   // testing
   // Lesson: using state to hold the innderWidth instead of variable. All other components will rerender 
@@ -1195,49 +1233,23 @@ const SettingsPage = () => {
 
   return (
     <Layout>
-      <header style={{
-        // to counter header in layout component bottom margin
-        marginTop: "-1.45rem",
-      }}>
-        <nav>
-          <ul
-            style={{
-              display: "flex",
-              gap: "2rem",
-              justifyContent: "end",
-              listStyleType: "none",
-              maxWidth: "963px",
-              margin: "auto",
-            }}
-          >
-            <li
-              style={{
-                padding: "0.25rem 0.5rem",
-                margin: "0",
-              }}
-            >
-              <Link to="/">Submit MWR</Link>
-            </li>
-            <li
-              style={{
-                padding: "0.25rem 0.5rem",
-                margin: "0",
-              }}
-            >
-              <Link to="/admin">Admin.</Link>
-            </li>
-            <li
-              style={{
-                textDecoration: "underline var(--red-btn-color-darker) 2px",
-                padding: "0.25rem 0.5rem",
-                margin: "0",
-              }}
-            >
-              <Link to="/settings">Settings</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <Navbar.Wrapper>
+        <Navbar.Items>
+
+          <Navbar.Item>
+            <Link to="/">Submit MWR</Link>
+          </Navbar.Item>
+
+          <Navbar.Item>
+            <Link to="/admin">Admin.</Link>
+          </Navbar.Item>
+
+          <Navbar.ItemIsActivePage>
+            <Link to="/settings">Settings</Link>
+          </Navbar.ItemIsActivePage>
+
+        </Navbar.Items>
+      </Navbar.Wrapper>
 
       <SettingsWrapperStyled>
         <ul className="settings-ul">{settingsMap}</ul>
