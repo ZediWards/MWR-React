70a00f67743643f4861b2761ad73c24fe6d748a8
zediwards <zediwards@protonmail.com>
2022-12-29 03:30:24 -0500

    login styles done

--- src/components/login.js
+++ src/components/login.js
@@ -2,6 +2,60 @@ import React from "react";
 import { navigate } from "gatsby";
 import { handleLogin, isLoggedIn } from "../services/auth";
 
+import styled from "styled-components";
+
+// lesson: styled components HAVE to be above component declaration
+const LoginContainerStyled = styled.div`
+width: fit-content;
+padding: 1rem;
+margin: auto;
+/* border: 1px solid black; */
+`
+
+const FormStyled = styled.form` 
+display: flex;
+flex-direction: column;
+gap: 1.25rem;
+
+label {
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+}
+
+/* lesson: customize input focus if want to */
+.input {
+  padding: 0.25rem;
+  border-radius: 10px;
+  &:focus {
+    /* outline: none; */
+    /* box-shadow: 0px 0px 5px #61C5FA; */
+    /* border:1px solid #5AB0DB; */
+  }
+}
+
+.submit {
+  margin-block-start: 1.25rem;
+  /* width: 100%; */
+  /* margin-bottom: 1rem; */
+  background-color: var(--green-btn-background);
+  border: 2px solid var(--btn-background-based-on-general);
+  box-shadow: 0px 2px 1px var(--gray-light);
+  color: var(--text-black);
+  transition: all 0.35s ease-Out;
+  cursor: pointer;
+  min-width: -webkit-min-content;
+  min-width: min-content;
+  padding: 0.5rem;
+  /* margin-inline: auto; */
+  /* font-size: 16px; */
+  border-radius: 10px;
+  /* display: block; */
+  &:hover {
+  background-color: var(--btn-background-based-on-general);
+  }
+}
+`
 class Login extends React.Component {
   state = {
     username: ``,
@@ -9,6 +63,7 @@ class Login extends React.Component {
   };
 
   handleUpdate = (event) => {
+    console.log(this.state)
     this.setState({
       [event.target.name]: event.target.value,
     });
@@ -24,10 +79,11 @@ class Login extends React.Component {
       navigate(`/app/profile`);
     }
 
+
     return (
-      <>
+      <LoginContainerStyled>
         <h1>Log in</h1>
-        <form
+        <FormStyled
           method="post"
           onSubmit={(event) => {
             this.handleSubmit(event);
@@ -36,19 +92,28 @@ class Login extends React.Component {
         >
           <label>
             Username
-            <input type="text" name="username" onChange={this.handleUpdate} />
+            <input
+              className="input"
+              type="text"
+              name="username"
+              onChange={this.handleUpdate} />
           </label>
+
           <label>
             Password
             <input
+              className="input"
               type="password"
               name="password"
               onChange={this.handleUpdate}
             />
           </label>
-          <input type="submit" value="Log In" />
-        </form>
-      </>
+          <input
+            className="submit"
+            type="submit"
+            value="Log In" />
+        </FormStyled>
+      </LoginContainerStyled>
     );
   }
 }
