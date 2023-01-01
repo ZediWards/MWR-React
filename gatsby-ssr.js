36344c8243bb590910461bd5d913c9396210f045
zediwards <zediwards@protonmail.com>
2023-01-01 04:11:35 -0500

    matched sr file with browser file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,13 @@
-// const React = require("react")
-// const GlobalContextProvider = require("./src/context/GlobalContextProvider").default
+const React = require("react")
+const GlobalContextProvider = require("./src/context/GlobalContextProvider").default
+const GlobalStylesDiv = require("./src/global_styles/globalStyledComponents").default
 
-// exports.wrapRootElement = ({ element }) => {
-//   return <GlobalContextProvider>{element}</GlobalContextProvider>
-// }
+
+exports.wrapRootElement = ({ element }) => {
+  return <GlobalContextProvider>
+    <GlobalStylesDiv>
+      {element}
+    </GlobalStylesDiv>
+  </GlobalContextProvider>
+}
 
